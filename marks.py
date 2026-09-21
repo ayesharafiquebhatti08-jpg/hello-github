d67baabda73e2a7cc50cd475953fbfc82ed3eb24
Ayesha Rafique Bhatti <ayesharafiquebhatti08@gmail.com>
2026-09-21 15:09:24 +0500

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter Your Marks: "))
+
+if marks >= 90:
+    print("GRADE A")
+elif marks >= 80:
+    print("GRADE B")
+elif marks >= 70:
+    print("GRADE C")
+else:
+    print("FAIL")
