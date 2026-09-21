d6e26b841ce32cbe2499d4ea4d8ed88c45d26d54
Ayesha Rafique Bhatti <ayesharafiquebhatti08@gmail.com>
2026-09-21 15:07:44 +0500

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter 1st Number: "))
+num2 = int(input("Enter 2nd Number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Remainder:", num1 % num2)
+print("Power:", num1 ** num2)
